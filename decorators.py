cfb20405707807da59bb0e16ee47af3be8dd423b
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-12-12 22:54:58 +0700

    Create decorators.py

---
+++ decorators.py
@@ -0,0 +1,10 @@
+from typing import Callable
+from pyrogram import Client
+from pyrogram.types import Message
+
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in OWNER_ID:
+            return await func(client, message)
+
+    return decorator
