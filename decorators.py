19d91e246a1fb7b93f3bdef0a6a2e6299f97875c
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-12-12 22:56:12 +0700

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,6 +1,7 @@
 from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
+from config import OWNER_ID
 
 def sudo_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
