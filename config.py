6b4a13d2d995c870f58eb15dc11971a22a51bd4d
idzeroid <88209926+idzero23@users.noreply.github.com>
2021-12-12 22:22:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", "1234"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@AntiUsersChannel_bot")
     DATABASE_URL = os.environ.get("DATABASE_URL")
